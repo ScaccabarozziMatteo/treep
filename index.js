beb273f76cbbd181d7a5d2dfb62f0192444f8b7a
Kristin Lilja <71507132+kristinlilja@users.noreply.github.com>
2022-04-20 21:45:18 +0200

    Initiating messaging
    following this page
    https://rnfirebase.io/messaging/usage

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// index.js
+// for the message function
+import messaging from '@react-native-firebase/messaging';
 
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
-
-
+// AppRegistry.registerComponent('app', () => App);
