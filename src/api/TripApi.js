e74d9a68345f65b965994854323f436edc46a035
Kristin Lilja <71507132+kristinlilja@users.noreply.github.com>
2022-04-28 14:17:40 +0200

    Branch working

--- src/api/TripApi.js
+++ src/api/TripApi.js
@@ -1,31 +1,40 @@
-import firestore from "@react-native-firebase/firestore";
-import storage from "@react-native-firebase/storage";
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import auth from '@react-native-firebase/auth';
+import {showToast} from '../utils/Utils';
 
 // Retrieves ALL the trips on the server
-export async function getAll () {
+export async function getAll() {
   let trips = [];
   //First call in order to retrieve all the info needed about the trips
-  const tripsData = (await firestore().collection("Trip").get()).docs;
+  const tripsData = (await firestore().collection('Trip').get()).docs;
 
   //For each trip, we also need to get the relative info about its user
   for (const t of tripsData) {
     //Calls firebase to get the user data
-    const userData = await firestore().collection("users/" + t.data().userID + "/public_info").doc("personal_data").get();
+    const userData = await firestore()
+      .collection('users/' + t.data().userID + '/public_info')
+      .doc('personal_data')
+      .get();
     //Merges together the info about the trip and the info about the user
     const mergedObj = {...t.data(), ...userData.data()};
     //Pushes the retrieved info into an array
     trips.push(mergedObj);
   }
-  return (trips);
+  return trips;
 }
 
 //Gets all the trips of a specified user
-export async function getUserTrips (userId) {
-  const data = await firestore().collection("Trip").where("userID", "==", userId).get()
-  if (data.empty)
-    return "";
-  else
+export async function getUserTrips(userId) {
+  const data = await firestore()
+    .collection('Trip')
+    .where('userID', '==', userId)
+    .get();
+  if (data.empty) {
+    return '';
+  } else {
     return data._docs;
+  }
 }
 
 // Gets the cover photo of a given trip
@@ -35,3 +44,12 @@ export async function getCoverPhoto(tripId) {
   return await reference.getDownloadURL();
 }
 
+//Changes the profile image of the current user
+export async function changeCoverImage(tripId) {
+  let url;
+  const imagePath = tripId.coverPhoto;
+  const reference = storage().ref(imagePath);
+  await reference.getDownloadURL(), (url = await reference.getDownloadURL());
+  await firestore().collection('Trip').doc('trip_data').update({photoURL: url});
+  await showToast('success', 'Upload', 'Cover photo uploaded!');
+}
--- src/pages/NewTripPage.js
+++ src/pages/NewTripPage.js
@@ -1,30 +1,10 @@
-import {HStack, View, VStack} from 'native-base';
-import {StyleSheet, Text, TextInput} from 'react-native';
-import React, {useState} from 'react';
+import {View} from 'native-base';
+import {StyleSheet} from 'react-native';
+import React from 'react';
 
 export default function NewTripPage() {
-  const [trips, setTrips] = useState({
-    description: '',
-    coverPhoto: '',
-    location: '',
-    photoURL: '',
-    title: '',
-    status: false,
-  });
-
   return (
     <View>
-      {/* Description */}
-      <VStack>
-        <HStack
-          alignItems={'center'}
-          justifyContent={'space-between'}
-          alignContent={'stretch'}>
-          <Text style={styles.title}>About me</Text>
-        </HStack>
-
-       
-      </VStack>
     </View>
   );
 }
